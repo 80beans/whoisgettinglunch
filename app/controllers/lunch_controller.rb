2d1872ed428b842d4c973e41ac485eed5bf3de3b
Robert Beekman <robertbeekman@noname.(none)>
2009-11-02 19:01:51 +0100

    added fancy js

--- app/controllers/lunch_controller.rb
+++ app/controllers/lunch_controller.rb
@@ -5,16 +5,18 @@ class LunchController < ApplicationController
   end
   
   def winner
-    @contestants = params[:contestants]
-    @winner = Lunch.get_random(@contestants)
+    if params[:contestants]
+      @contestants = params[:contestants]
+      @winner = Lunch.get_random(@contestants)
     
-    Justlogging.log("Winner: #{@winner}. Contestants #{@contestants * ','}")
+      Justlogging.log("Winner: #{@winner}. Contestants #{@contestants * ', '}")
     
-    respond_to do |format|
-      format.html 
-      format.xml
-      format.json
-      format.js
+      respond_to do |format|
+        format.html 
+        format.xml
+        format.json
+        format.js
+      end
     end
   end
 end
